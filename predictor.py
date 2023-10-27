c292630a46c81d89a54c5ec8088d3c707485df09
Victor is Geek <92975790+victorgeel@users.noreply.github.com>
2023-10-27 20:42:09 +0630

    Add files via upload

--- predictor.py
+++ predictor.py
@@ -18,16 +18,3 @@ def clear_predictor() -> None:
 
     PREDICTOR = None
 
-
-def predict_frame(target_frame: Frame) -> bool:
-    image = Image.fromarray(target_frame)
-    views = numpy.expand_dims(image, axis=0)
-    _, probability = get_predictor().predict(views)[0]
-    return probability > MAX_PROBABILITY
-
-
-def predict_image(target_path: str) -> bool:
-    
-
-def predict_video(target_path: str) -> bool:
-     return any(probability > MAX_PROBABILITY for probability in probabilities)
